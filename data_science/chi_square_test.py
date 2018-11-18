86fcd9d7fe55f82541ba4afcfe5485e1f1e0d3d9
Hao Yan <hao.yan@SN0203.local>
2018-11-19 01:25:35 +0900

    add the chi square sample

---
+++ data_science/chi_square_test.py
@@ -0,0 +1,12 @@
+"""
+chi squared test
+
+sampling distribution of the test statistic is a chi-squared distribution when null hypothesis is true
+
+evaluate how likely the observations that are made would be, assuming the null hypothesis is true.
+
+Pearson's chi-squared test: used to determine whether there is a significant between the expected frequencies and the observed frequencies in one or more categories.
+
+It is used to check the categorical data for ab test
+http://thestatsgeek.com/2013/07/22/ab-testing/
+"""
